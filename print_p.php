4934b22965a306fb8a389a340848c5d9c5f9f221
Daniel Gonzalez <oscar.d.gonzalez@gmail.com>
2016-09-11 10:28:03 -0500

    added header definition with preliminar css

--- print_p.php
+++ print_p.php
@@ -1,8 +1,163 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<title>Peritaje de Vehiculos</title>
 		<link rel="stylesheet" href="css/view.css">
+		<script type="text/javascript" src="http://d3js.org/d3.v3.min.js"></script>
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/signaturepad/jquery.signaturepad.min.js"></script>
+		<script type="text/javascript" src="js/signaturepad/json2.min.js"></script>
 	</head>
 	<body>
+		<div class="grid">
+			<div class="header">
+				<div class="row">
+					<div class="col-12">
+						<img src="http://placehold.it/255x40" alt="" />
+						<div class="col-1">
+							<h2>N. 2000</h2>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-12">
+						<h1>PERITAJE DE VEHICULOS USADOS</h1>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-4" id="hleft">
+						<div class="col-12">
+							<div class="col-6" style="float:left">
+								<h3 style="font-weight:bold">Fecha:</h3>
+							</div>
+							<div class="col-6" style="float:right">
+								<h3>10/09/2016</h3>
+							</div>
+						</div>
+						<div class="col-12">
+							<div class="col-6" style="float:left">
+								<h3 style="font-weight:bold">Asesor de servicio:</h3>
+							</div>
+							<div class="col-6" style="float:right">
+								<h3>Daniel Gonzalez</h3>
+							</div>
+						</div>
+					</div>
+					<div class="col-4" id="hcenter">
+						<div class="col-12">
+							<h3 style="font-weight:bold">DATOS DEL CLIENTE:</h3>
+						</div>
+						<div class="col-12">
+							<div class="col-4" style="float:left">
+								<h3 style="font-weight:bold">Nombre:</h3>
+							</div>
+							<div class="col-8" style="float:right">
+								<h3>Oscar Daniel Gonzalez</h3>
+							</div>
+						</div>
+						<div class="col-12">
+							<div class="col-4" style="float:left">
+								<h3 style="font-weight:bold">Cedula:</h3>
+							</div>
+							<div class="col-8" style="float:right">
+								<h3>80.136.753</h3>
+							</div>
+						</div>
+						<div class="col-12">
+							<div class="col-4" style="float:left">
+								<h3 style="font-weight:bold">Telefono:</h3>
+							</div>
+							<div class="col-8" style="float:right">
+								<h3>3186515096</h3>
+							</div>
+						</div>
+						<div class="col-12">
+							<div class="col-4" style="float:left">
+								<h3 style="font-weight:bold">Email:</h3>
+							</div>
+							<div class="col-8" style="float:right">
+								<h3>oscar.d.gonzalez@hotmail.com</h3>
+							</div>
+						</div>
+					</div>
+					<div class="col-4" id="hright">
+						<div class="col-12">
+							<h3 style="font-weight:bold">DATOS DEL VEHICULO:</h3>
+						</div>
+						<div class="col-12">
+							<div class="col-6" style="float:left">
+								<h3 style="font-weight:bold">Marca y linea:</h3>
+							</div>
+							<div class="col-6" style="float:right">
+								<h3>Renault Clio</h3>
+							</div>
+						</div>
+						<div class="col-12">
+							<div class="col-6" style="float:left">
+								<h3 style="font-weight:bold">Modelo:</h3>
+							</div>
+							<div class="col-6" style="float:right">
+								<h3>2008</h3>
+							</div>
+						</div>
+						<div class="col-12">
+							<div class="col-6" style="float:left">
+								<h3 style="font-weight:bold">Placa:</h3>
+							</div>
+							<div class="col-6" style="float:right">
+								<h3>CVL685</h3>
+							</div>
+						</div>
+						<div class="col-12">
+							<div class="col-6" style="float:left">
+								<h3 style="font-weight:bold">Kilometraje:</h3>
+							</div>
+							<div class="col-6" style="float:right">
+								<h3>77.000</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="content">
+				<div class="row">
+					<div class="col-4" id="cleft">
+						<table>
+							<caption>
+								A - Exterior:
+							</caption>
+							<thead>
+								<tr>
+									<th style="width: 45%" scope="col"><span style="display: none">1</span></th>
+									<th style="width: 45%" scope="col"><span style="display: none">Exterior:</span></th>
+						            <th  style="width: 15%" scope="col">B</th>
+									<th  style="width: 15%" scope="col">R</th>
+									<th  style="width: 15%" scope="col">M</th>
+									<th  style="width: 15%" scope="col">N/A</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td class="first_col">1</td>
+									<td class="first_col">Latoneria</td>
+						            <td><label style="display: none" >B</label><input name="matrix_1[1]" type="radio" value="1"></td>
+									<td><label style="display: none" >R</label><input name="matrix_1[1]" type="radio" value="2"></td>
+									<td><label style="display: none" >M</label><input name="matrix_1[1]" type="radio" value="3"></td>
+									<td><label style="display: none" >N/A</label><input name="matrix_1[1]" type="radio" value="4"></td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+					<div class="col-4" id="ccenter">
+						
+					</div>
+					<div class="col-4" id="cright">
+						
+					</div>
+				</div>
+			</div>
+		</div>
 		<div class="mockup-overlay">
 			<img src="img/Formato Peritaje.png">
 		</div>
